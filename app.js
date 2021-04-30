a320cfba6e39d40a8d8b67a5188e9c72207b2c2b
maddenlockin <maddenlockin@gmail.com>
2021-04-30 08:35:57 -0700

    initial commit

--- app.js
+++ app.js
@@ -1,5 +1,28 @@
 // import functions and grab DOM elements
+import { 
+    getFeelingImage,
+    getActivityImage
+} from './utils.js'
 
-// initialize state
+const feelingInput = document.getElementById('feeling-input');
+const activityInput = document.getElementById('activity-input');
+const button = document.getElementById('populate');
+const feelingImage = document.getElementById('feeling-image');
+const activityImage = document.getElementById('activity-image');
+
 
 // set event listeners to update state and DOM
+
+button.addEventListener('click', () => {
+    // post-click logic goes here!
+    console.log(getFeelingImage, getActivityImage, feelingInput, activityInput, feelingImage, activityImage);
+
+    const feelingLevel = feelingInput.value;
+    const activityLevel = activityInput.value;
+});
+
+
+
+
+
+
